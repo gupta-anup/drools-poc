c58d37d2fc26ad8193a15d5aed6e55c9c947ee1b
Anup Gupta <anup.gupta@nonstopio.com>
2025-04-15 14:56:04 +0530

    initialize drools project with Spring Boot setup and Maven wrapper

---
+++ src/main/java/com/poc/droolsprojectpoc/DroolsProjectPocApplication.java
@@ -0,0 +1,13 @@
+package com.poc.droolsprojectpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsProjectPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsProjectPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poc/droolsprojectpoc/DroolsProjectPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.droolsprojectpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsProjectPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
